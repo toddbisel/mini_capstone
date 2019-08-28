94f98ceb353b93c15a48c847912564afd8d6658e
Todd <todd.bisel@gmail.com>
2019-08-28 14:02:52 -0400

    Adds routes to display JSON for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,27 @@
 class Api::ProductsController < ApplicationController
+
+  def products_display
+    @products = Product.all
+    render 'products_display.json.jb'
+  end
+
+  def product_one
+    @product = Product.first
+    render 'product_one.json.jb'
+  end
+
+  def product_two
+    @product = Product.second
+    render 'product_two.json.jb'
+  end
+
+  def product_three
+    @product = Product.third
+    render 'product_three.json.jb'
+  end
+
+  def product_four
+    @product = Product.fourth
+    render 'product_four.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "products_url" => "products#products_display"
+    get "product_one" => "products#product_one"
+    get "product_two" => "products#product_two"
+    get "product_three" => "products#product_three"
+    get "product_four" => "products#product_four"
+  end
 end
